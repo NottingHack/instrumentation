277ea30c25b0729a425dd425010d1746ed44c120
dps.lwk@gmail.com <dps.lwk@gmail.com@8ae197d4-80ee-7899-219d-8f9980e4ea97>
2012-02-02 18:51:17 +0000

    timeout didnt work, flaw in logic

--- db/sp_temperature_check.sql
+++ db/sp_temperature_check.sql
@@ -13,8 +13,7 @@ BEGIN
 
   declare temperature_cur cursor for
     select  concat(t.name, ' : ',  cast(t.temperature as char(5)))
-    FROM temperature t
-    WHERE (NOW() - time) < 5*60;
+    FROM temperature t;
 
     
   declare continue HANDLER for not found set done = true;
