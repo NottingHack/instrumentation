ea04dfe8f7370502910ebe47c2034387c91d957d
hs@dswann.co.uk <hs@dswann.co.uk@8ae197d4-80ee-7899-219d-8f9980e4ea97>
2013-03-03 13:30:58 +0000

    Correct table.

--- db/tb_accounts.sql
+++ db/tb_accounts.sql
@@ -1,10 +1,9 @@
-drop table if exists accounts;
+drop table if exists account;
 
-create table accounts 
+create table account
 (
   account_id          int(11) not null auto_increment,
   payment_ref         varchar(18) not null,
-  rfid_serial         varchar(50),
   member_id           int(11) not null,
   primary key (account_id),
   unique key (payment_ref)
