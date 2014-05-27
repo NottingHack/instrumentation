7795d578f24d57177b222bd94020b3869433671c
James Hayward <jhayward1980@gmail.com>
2014-05-27 21:29:36 +0100

    Update tb_tl_tools.sql
    Calendar field for booking system

--- db/schema/tb_tl_tools.sql
+++ db/schema/tb_tl_tools.sql
@@ -12,6 +12,7 @@ create table tl_tools
   tool_booking_length int unsigned, -- default booking length for this tool
   tool_length_max     int unsigned, -- maximum amount of time a booking can be made for
   tool_bookings_max   int unsigned, -- maximum number of bookings a user can have at any one time
+  tool_calendar       varchar(255), -- id of google calendar
   primary key (tool_id),
   unique (tool_name)
 );
