04c22ac7aa96a80ac244d57775a294d2a0f7653e
Daniel <hs@dswann.co.uk>
2015-12-05 12:30:04 +0000

    update gatekeeper stats to use door id

--- web/stats/data/gatekeeper_members.json.php
+++ web/stats/data/gatekeeper_members.json.php
@@ -7,8 +7,8 @@ $sql = <<<SQL
     year(l.access_time) as Year, 
     monthname(l.access_time) as Month, 
     count(*) as "RFID entry",
-    (select count(*) from events e1         where year(e1.event_time)=year(l.access_time) and month(e1.event_time)=month(l.access_time) and e1.event_type = "DOORBELL" and (e1.event_value="INNER" or e1.event_value = "")) as "Inner doorbell rang",
-    (select count(*) from events e1         where year(e1.event_time)=year(l.access_time) and month(e1.event_time)=month(l.access_time) and e1.event_type = "DOORBELL" and e1.event_value="OUTER") as "Outer doorbell rang",
+    (select count(*) from events e1         where year(e1.event_time)=year(l.access_time) and month(e1.event_time)=month(l.access_time) and e1.event_type = "DOORBELL" and (e1.event_value="1" or e1.event_value = "")) as "Inner doorbell rang",
+    (select count(*) from events e1         where year(e1.event_time)=year(l.access_time) and month(e1.event_time)=month(l.access_time) and e1.event_type = "DOORBELL" and e1.event_value="2") as "Outer doorbell rang",
     (select count(*) from members m         where year(m.join_date  )=year(l.access_time) and month(m.join_date  )=month(l.access_time)) as "New members"
   from access_log l
   where l.access_result = 20
