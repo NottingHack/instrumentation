6ad2cc3c3d3568c6e34fac498f1b69347d111e95
dpslwk <dps.lwk@gmail.com>
2019-07-20 10:55:27 +0100

    Fix the other to stats pages

--- web/stats/data/gatekeeper_members.json.php
+++ web/stats/data/gatekeeper_members.json.php
@@ -3,7 +3,7 @@
 include "dbaccess_header.php";
 
 
-$start_date  = new DateTime("2010-01-01"); 
+$start_date  = new DateTime("2010-01-01");
 $end_date    = new DateTime();
 $date_diff   = $start_date->diff($end_date);
 $month_count = $date_diff->m + ($date_diff->y * 12);
@@ -14,17 +14,17 @@ $sql = <<<SQL
 select
   year(dt.report_date) as Year,
   monthname(dt.report_date) as Month,
-  ifnull(new_mem.c, (select count(*) from members m where m.join_date >= dt.report_date and m.join_date < date_add(dt.report_date, interval 1 month))) as "New members",
+  ifnull(new_mem.c, (select count(*) from profile p where p.join_date >= dt.report_date and p.join_date < date_add(dt.report_date, interval 1 month))) as "New members",
   ifnull(ex_mem.c, 0)   as "Ex members",
   ifnull(rfid.c, 0)     as "RFID entry",
   ifnull(in_door.c, 0)  as "Inner doorbell rang",
   ifnull(out_door.c, 0) as "Outer doorbell rang"
-from 
+from
 (
 SQL;
 $sql .= "select date_add('" . $start_date->format('Y-m-d') . "', interval @num:=@num+1 month) as report_date,";
 $sql .= "  cast(@num as signed) as i ";
-$sql .= "  from members, (select @num:=-1) num "; // choice of members table is arbitrary, it just needs to be a table with at least $month_count rows in it
+$sql .= "  from user, (select @num:=-1) num "; // choice of members table is arbitrary, it just needs to be a table with at least $month_count rows in it
 $sql .= "  limit $month_count ";
 $sql .= <<<SQL
 ) as dt
@@ -34,53 +34,55 @@ left outer join
     year(l.access_time) as Year,
     monthname(l.access_time) as Month,
     count(*) as c
-  from access_log l
+  from access_logs l
   where l.access_result = 20 -- access granted
   group by year(l.access_time), monthname(l.access_time)
 ) rfid on rfid.Year = year(dt.report_date) and rfid.Month = monthname(dt.report_date)
 left outer join
 ( -- inner door bell
   select
-    year(e.event_time) as Year,
-    monthname(e.event_time) as Month,
+    year(e.time) as Year,
+    monthname(e.time) as Month,
     count(*) as c
   from events e
-  where e.event_type = "DOORBELL"
-    and (e.event_value="1" or e.event_value = "")
-  group by year(e.event_time), monthname(e.event_time)
+  where e.type = "DOORBELL"
+    and (e.value="1" or e.value = "")
+  group by year(e.time), monthname(e.time)
 ) in_door on in_door.Year = year(dt.report_date) and in_door.Month = monthname(dt.report_date)
 left outer join
 ( -- outer door bell
   select
-    year(e.event_time) as Year,
-    monthname(e.event_time) as Month,
+    year(e.time) as Year,
+    monthname(e.time) as Month,
     count(*) as c
   from events e
-  where e.event_type = "DOORBELL"
-    and (e.event_value="2")
-  group by year(e.event_time), monthname(e.event_time)
+  where e.type = "DOORBELL"
+    and (e.value="2")
+  group by year(e.time), monthname(e.time)
 ) out_door on out_door.Year = year(dt.report_date) and out_door.Month = monthname(dt.report_date)
 left outer join
 ( -- new members (post HMS intoduction)
   select
-    year(s.timestamp) as Year,
-    monthname(s.timestamp) as Month,
+    year(ru.created_at) as Year,
+    monthname(ru.created_at) as Month,
     count(*) as c
-  from status_updates s
-  where s.old_status != 5
-    and s.new_status = 5
-  group by year(s.timestamp), monthname(s.timestamp)
+  from role_updates ru
+    left join roles ra on (ra.id = ru.added_role_id)
+    left join roles rr on (rr.id = ru.removed_role_id)
+  where ra.name = 'member.current'
+  group by year(ru.created_at), monthname(ru.created_at)
 ) new_mem on new_mem.Year = year(dt.report_date) and new_mem.Month = monthname(dt.report_date)
 left outer join
 ( -- ex members
   select
-    year(s.timestamp) as Year,
-    monthname(s.timestamp) as Month,
+    year(ru.created_at) as Year,
+    monthname(ru.created_at) as Month,
     count(*) as c
-  from status_updates s
-  where s.old_status = 5
-    and s.new_status = 6
-  group by year(s.timestamp), monthname(s.timestamp)
+  from role_updates ru
+    left join roles ra on (ra.id = ru.added_role_id)
+    left join roles rr on (rr.id = ru.removed_role_id)
+  where ra.name = 'member.ex'
+  group by year(ru.created_at), monthname(ru.created_at)
 ) ex_mem on ex_mem.Year = year(dt.report_date) and ex_mem.Month = monthname(dt.report_date)
 order by dt.i desc;
 SQL;
--- web/stats/data/member_stats.json.php
+++ web/stats/data/member_stats.json.php
@@ -3,107 +3,110 @@
 include "dbaccess_header.php";
 
 $sql = <<<SQL
-  SELECT 
+  SELECT
     COUNT(DISTINCT stats.last_day) AS last_day,
     COUNT(DISTINCT stats.last_week) AS last_week,
     COUNT(DISTINCT stats.last_month) AS last_month,
     COUNT(DISTINCT stats.last_quarter) AS last_quarter,
     COUNT(DISTINCT stats.last_year) AS last_year,
     COUNT(DISTINCT stats.anytime) AS anytime,
-    (SELECT COUNT(*) FROM members WHERE members.member_status = 5) AS total_current_members
+    (SELECT COUNT(*) FROM user u LEFT JOIN role_user ru ON u.id = ru.user_id LEFT JOIN roles r ON ru.role_id = r.id WHERE r.name = 'member.current') AS total_current_members
 
   FROM (
 
   -- Current members accessing the space
   SELECT
-    IF(access_log.access_time >= DATE_SUB(NOW(), INTERVAL 1 DAY), members.member_id, NULL) AS last_day,  
-    IF(access_log.access_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK), members.member_id, NULL) AS last_week,
-    IF(access_log.access_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH), members.member_id, NULL) AS last_month,
-    IF(access_log.access_time >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), members.member_id, NULL) AS last_quarter,
-    IF(access_log.access_time >= DATE_SUB(NOW(), INTERVAL 1 YEAR), members.member_id, NULL) AS last_year,
-    members.member_id AS anytime
-    
+    IF(al.access_time >= DATE_SUB(NOW(), INTERVAL 1 DAY), u.id, NULL) AS last_day,
+    IF(al.access_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK), u.id, NULL) AS last_week,
+    IF(al.access_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH), u.id, NULL) AS last_month,
+    IF(al.access_time >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), u.id, NULL) AS last_quarter,
+    IF(al.access_time >= DATE_SUB(NOW(), INTERVAL 1 YEAR), u.id, NULL) AS last_year,
+    u.id AS anytime
+
   FROM
-    members
-    JOIN access_log ON members.member_id = access_log.member_id
+    user u
+    LEFT JOIN role_user ru ON u.id = ru.user_id LEFT JOIN roles r ON ru.role_id = r.id
+    JOIN access_logs al ON u.id = al.user_id
     JOIN
     (
       SELECT
-        access_log.member_id,
-        MAX(access_log.access_time) max_access_time
+        al.user_id,
+        MAX(al.access_time) max_access_time
       FROM
-        access_log
+        access_logs al
       GROUP BY
-        access_log.member_id
-    ) max_access_log ON
-    access_log.member_id = max_access_log.member_id
+        al.user_id
+    ) max_al ON
+    al.user_id = max_al.user_id
     AND
-    access_log.access_time = max_access_log.max_access_time
-    
+    al.access_time = max_al.max_access_time
+
   WHERE
-    members.member_status = 5
+    r.name = 'member.current'
 
   UNION
 
   -- Current members using an access controlled tool
   SELECT
-    IF(tl_tool_usages.usage_start >= DATE_SUB(NOW(), INTERVAL 1 DAY), members.member_id, NULL) AS last_day,  
-    IF(tl_tool_usages.usage_start >= DATE_SUB(NOW(), INTERVAL 1 WEEK), members.member_id, NULL) AS last_week,
-    IF(tl_tool_usages.usage_start >= DATE_SUB(NOW(), INTERVAL 1 MONTH), members.member_id, NULL) AS last_month,
-    IF(tl_tool_usages.usage_start >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), members.member_id, NULL) AS last_quarter,
-    IF(tl_tool_usages.usage_start >= DATE_SUB(NOW(), INTERVAL 1 YEAR), members.member_id, NULL) AS last_year,
-    members.member_id AS anytime
-    
+    IF(tool_usages.start >= DATE_SUB(NOW(), INTERVAL 1 DAY), u.id, NULL) AS last_day,
+    IF(tool_usages.start >= DATE_SUB(NOW(), INTERVAL 1 WEEK), u.id, NULL) AS last_week,
+    IF(tool_usages.start >= DATE_SUB(NOW(), INTERVAL 1 MONTH), u.id, NULL) AS last_month,
+    IF(tool_usages.start >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), u.id, NULL) AS last_quarter,
+    IF(tool_usages.start >= DATE_SUB(NOW(), INTERVAL 1 YEAR), u.id, NULL) AS last_year,
+    u.id AS anytime
+
   FROM
-    members
-    JOIN tl_tool_usages ON members.member_id = tl_tool_usages.member_id
+    user u
+    LEFT JOIN role_user ru ON u.id = ru.user_id LEFT JOIN roles r ON ru.role_id = r.id
+    JOIN tool_usages ON u.id = tool_usages.user_id
     JOIN
     (
       SELECT
-        tl_tool_usages.member_id,
-        MAX(tl_tool_usages.usage_start) max_usage_start
+        tool_usages.user_id,
+        MAX(tool_usages.start) max_start
       FROM
-        tl_tool_usages
+        tool_usages
       GROUP BY
-        tl_tool_usages.member_id
-    ) max_tl_tool_usages ON
-    tl_tool_usages.member_id = max_tl_tool_usages.member_id
+        tool_usages.user_id
+    ) max_tool_usages ON
+    tool_usages.user_id = max_tool_usages.user_id
     AND
-    tl_tool_usages.usage_start = max_tl_tool_usages.max_usage_start
-    
+    tool_usages.start = max_tool_usages.max_start
+
   WHERE
-    members.member_status = 5
+    r.name = 'member.current'
 
   UNION
 
   -- Current members using the vending machine or note acceptor
   SELECT
-    IF(vend_log.enq_datetime >= DATE_SUB(NOW(), INTERVAL 1 DAY), members.member_id, NULL) AS last_day,  
-    IF(vend_log.enq_datetime >= DATE_SUB(NOW(), INTERVAL 1 WEEK), members.member_id, NULL) AS last_week,
-    IF(vend_log.enq_datetime >= DATE_SUB(NOW(), INTERVAL 1 MONTH), members.member_id, NULL) AS last_month,
-    IF(vend_log.enq_datetime >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), members.member_id, NULL) AS last_quarter,
-    IF(vend_log.enq_datetime >= DATE_SUB(NOW(), INTERVAL 1 YEAR), members.member_id, NULL) AS last_year,
-    members.member_id AS anytime
-    
+    IF(vend_logs.enqueued_time >= DATE_SUB(NOW(), INTERVAL 1 DAY), u.id, NULL) AS last_day,
+    IF(vend_logs.enqueued_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK), u.id, NULL) AS last_week,
+    IF(vend_logs.enqueued_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH), u.id, NULL) AS last_month,
+    IF(vend_logs.enqueued_time >= DATE_SUB(NOW(), INTERVAL 1 QUARTER), u.id, NULL) AS last_quarter,
+    IF(vend_logs.enqueued_time >= DATE_SUB(NOW(), INTERVAL 1 YEAR), u.id, NULL) AS last_year,
+    u.id AS anytime
+
   FROM
-    members
-    JOIN vend_log ON members.member_id = vend_log.member_id
+    user u
+    LEFT JOIN role_user ru ON u.id = ru.user_id LEFT JOIN roles r ON ru.role_id = r.id
+    JOIN vend_logs ON u.id = vend_logs.user_id
     JOIN
     (
       SELECT
-        vend_log.member_id,
-        MAX(vend_log.enq_datetime) max_enq_datetime
+        vend_logs.user_id,
+        MAX(vend_logs.enqueued_time) max_enqueued_time
       FROM
-        vend_log
+        vend_logs
       GROUP BY
-        vend_log.member_id
-    ) max_vend_log ON
-    vend_log.member_id = max_vend_log.member_id
+        vend_logs.user_id
+    ) max_vend_logs ON
+    vend_logs.user_id = max_vend_logs.user_id
     AND
-    vend_log.enq_datetime = max_vend_log.max_enq_datetime
-    
+    vend_logs.enqueued_time = max_vend_logs.max_enqueued_time
+
   WHERE
-    members.member_status = 5
+    r.name = 'member.current'
 
   ) stats;
 SQL;
